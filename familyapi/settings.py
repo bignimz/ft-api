7791b0e8d27efa2e80ac30c3dca9d470aec8c2ae
[alroude] <[nimrod.chitayi@gmail.com]>
2024-06-11 00:12:40 +0300

    Database settings updated

--- familyapi/settings.py
+++ familyapi/settings.py
@@ -109,19 +109,19 @@ WSGI_APPLICATION = 'familyapi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL')
+#     )
+# }
 
-if os.environ.get('ENV') == 'LOCAL':
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 # DATABASES = {
 #     'default': {
