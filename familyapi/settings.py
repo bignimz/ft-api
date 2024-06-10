1d8473f2a42b3aff21c35f006dce848b160cae3a
[alroude] <[nimrod.chitayi@gmail.com]>
2024-06-10 23:12:17 +0300

    Set STATIC_ROOT

--- familyapi/settings.py
+++ familyapi/settings.py
@@ -167,6 +167,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
